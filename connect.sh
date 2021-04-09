ff16697a4d712a47c3b76ceafdc4dab074ea7b4d
neoaman <amanthe001@gmail.com>
2021-04-09 15:34:48 +0530

    fix: setup.sh is fixed

--- connect.sh
+++ connect.sh
@@ -1,1 +1,1 @@
-ssh -i "/media/aman/Study/WEBSITES/Credential/solution.pem" ubuntu@ec2-34-220-236-221.us-west-2.compute.amazonaws.com
+ssh -i "/media/aman/Study/WEBSITES/Credential/solution.pem" ubuntu@ec2-54-71-63-48.us-west-2.compute.amazonaws.com
--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 sudo apt update
 sudo apt -y upgrade
-sudo apt-get install -y python3.8
+sudo apt-get -y install python3.8
 sudo ln -sT /usr/bin/python3 /usr/bin/python
-sudo apt install -y python3-pip
+sudo apt install python3-pip
 sudo ln -sT /usr/bin/pip3 /usr/bin/pip
-sudo apt-get install apache2
-sudo apt-get install libapache2-mod-wsgi-py3
+sudo apt-get -y install apache2
+sudo apt-get -y install libapache2-mod-wsgi-py3
 sudo pip3 install virtualenv
-sudo virtualenv /home/ubuntu/venv/sol
+sudo su
+virtualenv /home/ubuntu/venv/sol
 source /home/ubuntu/venv/sol/bin/activate
-sudo pip install -r requirements.txt
+pip install -r requirements.txt
+sudo chown :www-data db.sqlite3
+sudo chown www-data:www-data /var/www/solution
+python manage.py migrate
