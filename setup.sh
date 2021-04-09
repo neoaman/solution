ac8dcfe629a2baf251ec658b60cb64c7a7267e33
neoaman <amanthe001@gmail.com>
2021-04-09 15:55:07 +0530

    mod: modification in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,22 @@ sudo apt update
 sudo apt -y upgrade
 sudo apt-get -y install python3.8
 sudo ln -sT /usr/bin/python3 /usr/bin/python
-sudo apt install python3-pip
+sudo apt -y install python3-pip
 sudo ln -sT /usr/bin/pip3 /usr/bin/pip
 sudo apt-get -y install apache2
 sudo apt-get -y install libapache2-mod-wsgi-py3
 sudo pip3 install virtualenv
-sudo su
 virtualenv /home/ubuntu/venv/sol
 source /home/ubuntu/venv/sol/bin/activate
 pip install -r requirements.txt
 sudo chown :www-data db.sqlite3
 sudo chown www-data:www-data /var/www/solution
+sudo chown www-data:www-data /var/www/solution/
+sudo chown www-data:www-data /var/www/solution/*
 python manage.py migrate
+cp custom.conf /etc/apache2/sites-available/
+cd /etc/apache2/sites-available/
+a2ensite custom.conf
+a2dissite default-ssl.conf
+a2dissite 000-default.conf
+service apache2 restart
